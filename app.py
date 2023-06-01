8a6284e8a1d2f0f52725658209f89a4cb801aeb0
Amoslitw <103892754+Amoslitw@users.noreply.github.com>
2023-06-01 17:33:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def callback():
         result = pn.read ( event.message.text )
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=)
+            TextSendMessage(text=result)
         )
 
     return 'OK'
