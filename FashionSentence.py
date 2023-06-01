c99aa5ddc9d2ee2649b91be2d3073468b19d2bd2
Amoslitw <103892754+Amoslitw@users.noreply.github.com>
2023-06-01 17:53:30 +0800

    Update FashionSentence.py

--- FashionSentence.py
+++ FashionSentence.py
@@ -10,6 +10,12 @@ def read ( word ):
     data = bs.find_all('p')
     try:
         rslt= data[0].text
-        return ( rslt )
+        
+        if '沒有內容' in rslt :
+            return ('找不到相關資料，請重試。')
+        elif '需要編修' in rslt:
+            return ('該資料正在建立中。')
+        else:
+            return( rslt)
     except:
         return( '查無此字' )
